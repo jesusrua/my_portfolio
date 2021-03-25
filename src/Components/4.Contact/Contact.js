36999ffae0836ee3c91021f826b813b07d4b21bd
Jesus Rua <jesus.rua.pareja@gmail.com>
2021-03-25 09:36:05 +0100

    buttons styled

--- src/Components/4.Contact/Contact.js
+++ src/Components/4.Contact/Contact.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import './Contact.css';
-import Styles from './Contact.css';
 import {Container, Row, Col, Form, Button} from 'react-bootstrap';
 import {FontAwesomeIcon} from '@fortawesome/react-fontawesome';
 import {faLinkedin, faGithubSquare} from '@fortawesome/free-brands-svg-icons';
